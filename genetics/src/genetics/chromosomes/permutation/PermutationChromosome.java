65d04d2a9ac48a0a11aae2fc85cfa339c873bb6e
Andrija Dukic <andrija.dukic@fer.hr>
2021-02-14 14:59:28 +0100

    started work on permutation chromosomes

---
+++ genetics/src/genetics/chromosomes/permutation/PermutationChromosome.java
@@ -0,0 +1,10 @@
+package genetics.chromosomes.permutation;
+
+import genetics.chromosomes.AbstractChromosome;
+
+public abstract class PermutationChromosome extends AbstractChromosome<PermutationGenotype> {
+
+    protected PermutationChromosome(PermutationGenotype genotype) {
+        super(genotype);
+    }
+}
---
+++ genetics/src/genetics/chromosomes/permutation/PermutationGenotype.java
@@ -0,0 +1,44 @@
+package genetics.chromosomes.permutation;
+
+import genetics.chromosomes.RawGenotype;
+import genetics.chromosomes.floatingpoint.FloatingPointGenotype;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class PermutationGenotype extends RawGenotype<int[]> {
+
+    public PermutationGenotype(int[] raw) {
+        super(raw);
+    }
+
+    @Override
+    public PermutationGenotype copy() {
+        int[] rawCopy = new int[raw.length];
+        System.arraycopy(raw, 0, rawCopy, 0, rawCopy.length);
+        return new PermutationGenotype(rawCopy);
+    }
+
+    @Override
+    public PermutationGenotype newInstance(int[] raw) {
+        return new PermutationGenotype(raw);
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.stream(raw).mapToObj(String::valueOf).collect(Collectors.joining(","));
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(raw);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PermutationGenotype that = (PermutationGenotype) o;
+        return Arrays.equals(raw, that.raw);
+    }
+}
